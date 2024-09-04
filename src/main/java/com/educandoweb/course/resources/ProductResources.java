d6eb97951e43e42f0a1f1f57931d4beec90c2c64
joao-lourencon <joaolouren478@gmail.com>
2024-09-03 23:29:21 -0300

    Product

--- src/main/java/com/educandoweb/course/resources/ProductResources.java
+++ src/main/java/com/educandoweb/course/resources/ProductResources.java
@@ -1,8 +1,7 @@
 package com.educandoweb.course.resources;
 
-import com.educandoweb.course.entities.Order;
-import com.educandoweb.course.entities.User;
-import com.educandoweb.course.services.OrderService;
+import com.educandoweb.course.entities.Product;
+import com.educandoweb.course.services.ProductServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,24 +9,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/orders")
-public class OrderResource {
+@RequestMapping(value = "/products")
+public class ProductResources {
+
     @Autowired
-    private OrderService service;
+    public ProductServices services;
 
     @GetMapping
-    public ResponseEntity<List<Order>> findAll() {
-        List<Order> list = service.findAll();
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = services.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Order> findById(@PathVariable Long id) {
-        Order order = service.findByid(id);
-        return ResponseEntity.ok().body(order);
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Product product = services.findById(id);
+        return ResponseEntity.ok().body(product);
     }
 }
