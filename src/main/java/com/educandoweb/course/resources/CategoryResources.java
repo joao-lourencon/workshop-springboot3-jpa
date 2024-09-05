f27e0596f45e99474901bdcfa1220b1dbd6b1932
joao-lourencon <joaolouren478@gmail.com>
2024-09-04 23:26:40 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/resources/CategoryResources.java
+++ src/main/java/com/educandoweb/course/resources/CategoryResources.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categorys")
+@RequestMapping(value = "/categories")
 public class CategoryResources{
         @Autowired
         private CategoryService service;
